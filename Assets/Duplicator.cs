2298829ab364e411a0ffb7f9981da92923b8f9a0
Caleb Seay <s179067@students.aie.edu.au>
2018-01-08 16:16:33 -0800

    Add initial commit

---
+++ Assets/Duplicator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duplicator : MonoBehaviour {
+    public GameObject pickup;
+    public float range;
+    public float spawnInterval;
+    float spawnTimer;
+	// Use this for initialization
+	void Start () {
+        spawnTimer = 0;
+	}
+	
+    void spawnPickup()
+    {
+        GameObject spawnedPickUp = Instantiate(pickup);
+        float randX = Random.Range(-range, range);
+        float randZ = Random.Range(-range, range);
+        spawnedPickUp.transform.position = transform.position + new Vector3(randX, 0, randZ);
+        spawnedPickUp.GetComponent<PickUp>().scoreAdded = Random.Range(-5, 30);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            spawnTimer = spawnInterval;
+            spawnPickup();
+        }
+	}
+}
---
+++ Assets/PickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    public int scoreAdded;
+    public float rotationSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed, 0, rotationSpeed / 3);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var playerController = other.GetComponent<BallController>();
+            if (playerController != null)
+            {
+                playerController.score += scoreAdded;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
