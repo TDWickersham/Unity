01dd2119941710cf9a154869c6bdc937918da2d1
Caleb Seay <s179067@students.aie.edu.au>
2018-01-09 17:58:44 -0800

    did stuff

---
+++ Assets/Grow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grow : MonoBehaviour
+{
+
+    public float rotationSpeed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotationSpeed, 0, rotationSpeed / 3);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            var playerController = other.GetComponent<BallController>();
+            if (playerController != null)
+            {
+               
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/PointGate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointGate : MonoBehaviour {
+
+    public int targetScore;
+    public BallController player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = FindObjectOfType<BallController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (player.score >= targetScore)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+}
