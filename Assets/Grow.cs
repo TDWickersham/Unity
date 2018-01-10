572842b718ffacba142c967ccf2a6dc8be7fd18d
Caleb Seay <s179067@students.aie.edu.au>
2018-01-10 12:03:52 -0800

    pew

--- Assets/Grow.cs
+++ Assets/Grow.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class Grow : MonoBehaviour
 {
-
+    public float timer;
+    public Vector3 scale;
     public float rotationSpeed;
+
+    IEnumerator CoroutineScale(GameObject player)
+    {
+        while(transform.localScale != scale)
+        {
+            player.transform.localScale += Vector3.one;
+
+            yield return new WaitForSeconds(timer);
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -25,7 +37,7 @@ public class Grow : MonoBehaviour
             var playerController = other.GetComponent<BallController>();
             if (playerController != null)
             {
-               
+                StartCoroutine(CoroutineScale(other.gameObject));
                 Destroy(gameObject);
             }
         }
