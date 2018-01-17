2df18ff7131756bf9edde3b83b268b29711a1e15
Caleb Seay <s179067@students.aie.edu.au>
2018-01-16 17:25:31 -0800

    scoring

---
+++ Assets/ScoreDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text scoreLabel;
+    public BallController player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = FindObjectOfType<BallController>();
+        scoreLabel = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        scoreLabel.text = player.score.ToString();
+	}
+}
